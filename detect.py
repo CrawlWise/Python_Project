2a9019edbf198c6bc2f038ed194e0c68129dea17
unknown <damtob4rill@gmail.com>
2023-04-10 17:29:27 +0100

    update

--- detect.py
+++ detect.py
@@ -1,20 +1,28 @@
 import cv2, glob
 
+#Load the images
 all_images = glob.glob("*.jpg")
+
+# Load the face detector
 detect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+# Detects many faces of different sizes in the input image
 for image in all_images:
- # Detects many faces of different sizes in the input image
-    img = cv2.imread(image)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detect.detectMultiScale(gray_img, 1.1, 5)
+     img = cv2.imread(image)
 
-    for (x, y, w, h) in faces:
-# To draw rectangle in the different faces
-        final_img = cv2.rectangle(img, (x,y), (x+w,y+h), (500, 500, 0), 2)
+# Convert to grayscale
+     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("Face Detection", final_img)
-    cv2.waitKey(2000)
+# Detect faces
+     faces = detect.detectMultiScale(gray_img, 1.1, 5)
+
+     for (x, y, w, h) in faces:
+# To draw rectangle around different faces
+         final_img = cv2.rectangle(img, (x,y), (x+w,y+h), (0, 280, 0), 3)
+
+#Show the result
+     cv2.imshow("Face Detection", final_img)
+     cv2.waitKey(2000)
 
 # Close the window
-    cv2.destroyAllWindows
+     cv2.destroyAllWindows
